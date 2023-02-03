f98f92326ee17eccc6a50ec0353e4a028f656c29
wed-4 <94733396+wed-4@users.noreply.github.com>
2023-02-03 11:28:31 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let sleep_time = 0
 input.onButtonPressed(Button.A, function () {
     sleep_time = sleep_time - 50
     if (sleep_time < 0) {
@@ -10,4 +9,12 @@ input.onButtonPressed(Button.B, function () {
     if (500 < sleep_time) {
         sleep_time = 500
     }
+})
+let sleep_time = 0
+sleep_time = 100
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(sleep_time)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(sleep_time)
 })
